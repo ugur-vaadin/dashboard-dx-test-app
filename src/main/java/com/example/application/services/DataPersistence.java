a14ca80883db16a3953b3526b9fa3a13ce5e0e3f
ugur-vaadin <ugur@vaadin.com>
2024-11-06 09:18:59 +0200

    chore: remove unused api

--- src/main/java/com/example/application/services/DataPersistence.java
+++ src/main/java/com/example/application/services/DataPersistence.java
@@ -3,9 +3,6 @@ package com.example.application.services;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
-import elemental.json.Json;
-import elemental.json.JsonArray;
-import elemental.json.JsonObject;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
@@ -19,24 +16,14 @@ public class DataPersistence implements Serializable {
 
     private List<SerializableDashboardItem> items;
 
-    private String jsonItems;
-
     private DataPersistence() {
         initItems();
-        initJsonItems();
-    }
-
-    public void storeJsonItems(String jsonItems) {
-        this.jsonItems = jsonItems;
     }
 
     public void storeItems(List<SerializableDashboardItem> items) {
         this.items = items;
     }
 
-    public String getJsonItems() {
-        return jsonItems;
-    }
 
     public List<SerializableDashboardItem> getItems() {
         return items;
@@ -125,46 +112,5 @@ public class DataPersistence implements Serializable {
         widgetWithTextField.setWidgetId("server-widget-with-text-field");
         widgetWithTextField.setImportantData("Initial important data");
         items.add(widgetWithTextField);
-    }
-
-    private void initJsonItems() {
-        JsonArray jsonItemsArray = Json.createArray();
-
-        JsonObject item1 = Json.createObject();
-        item1.put("id", "client-widget-1");
-        item1.put("title", "Widget 1");
-        item1.put("type", "column");
-        item1.put("importantData", "Important 1");
-        jsonItemsArray.set(0, item1);
-
-        JsonObject item2 = Json.createObject();
-        item2.put("id", "client-widget-2");
-        item2.put("title", "Widget 2");
-        item2.put("type", "line");
-        item2.put("importantData", "Important 2");
-        jsonItemsArray.set(1, item2);
-
-        JsonObject section = Json.createObject();
-        section.put("id", "client-section");
-        section.put("title", "Section");
-        JsonArray sectionItems = Json.createArray();
-        section.put("items", sectionItems);
-        jsonItemsArray.set(2, section);
-
-        JsonObject itemInSection1 = Json.createObject();
-        itemInSection1.put("id", "client-widget-3");
-        itemInSection1.put("title", "Widget in section 1");
-        itemInSection1.put("type", "column");
-        itemInSection1.put("importantData", "Important 3");
-        sectionItems.set(0, itemInSection1);
-
-        JsonObject itemInSection2 = Json.createObject();
-        itemInSection2.put("id", "client-widget-4");
-        itemInSection2.put("title", "Widget in section 2");
-        itemInSection2.put("type", "line");
-        itemInSection2.put("importantData", "Important 4");
-        sectionItems.set(1, itemInSection2);
-
-        jsonItems = jsonItemsArray.toJson();
     }
 }
